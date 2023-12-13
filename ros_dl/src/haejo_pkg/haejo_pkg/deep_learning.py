e9b1d5f715fab2131829d86bd5485699692d210d
beanwork <isumin138@gmail.com>
2023-12-13 15:27:41 +0900

    feat: 문 전등 과자 인식 모델 그리고 deep_learning.py 수정ls

--- ros_dl/src/haejo_pkg/haejo_pkg/deep_learning.py
+++ ros_dl/src/haejo_pkg/haejo_pkg/deep_learning.py
@@ -1,16 +1,8 @@
 import cv2
 import rclpy
-import numpy as np
-import torch.nn as nn
-import torch
-import mediapipe as mp
+import configparser
 import sys
-import time
 
-from torch.utils.data import Dataset, DataLoader
-from ultralytics import YOLO
-from ultralytics.utils.plotting import Annotator
-from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
@@ -21,196 +13,19 @@ from PyQt5.QtCore import *
 
 from . import data_manager
 from datetime import datetime as dt
-from haejo_pkg.modules import DetectDesk
 from haejo_pkg.utils import Logger
 from haejo_pkg.utils.ConfigUtil import get_config
 
-config = get_config()
-from_class = uic.loadUiType(config['GUI'])[0]
+from haejo_pkg.modules.detect_door import Detectdoor
+from haejo_pkg.modules.detect_light import Detectlight
+from haejo_pkg.modules.detect_phone import DetectPhone
+from haejo_pkg.modules.detect_snack import Detectsnack
+from haejo_pkg.modules.detect_desk import DetectDesk
 
 log = Logger.Logger('haejo_deep_learning.log')
+config = get_config()
 
-mp_pose = mp.solutions.pose
-mp_pose_pose = mp_pose.Pose(static_image_mode=False, model_complexity=1,
-                            enable_segmentation = False, min_detection_confidence=0.7)
-xy_list_list = []
-
-attention_dot = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-draw_line = [[0, 4], [0, 1], [4, 5], [1, 2], [5, 6], [2, 3], [6, 8], [3, 7], [9, 10]]
-
-
-class MyDataset(Dataset):
-    def __init__(self, seq_list):
-        self.X = []
-        self.y = []
-        for dic in seq_list:
-            self.y.append(dic['key'])
-            self.X.append(dic['value'])
-        
-    def __getitem__(self, index):
-        data = self.X[index]
-        label = self.y[index]
-        return torch.Tensor(np.array(data)), torch.tensor(np.array(int(label)))
-    
-    def __len__(self):
-        return len(self.X)
-
-
-class skeleton_LSTM(nn.Module):
-    def __init__(self):
-        super(skeleton_LSTM, self).__init__()
-        self.lstm1 = nn.LSTM(input_size= 26, hidden_size=128, num_layers=1, batch_first=True)
-        self.lstm2 = nn.LSTM(input_size=128, hidden_size=256, num_layers=1, batch_first=True)
-        self.lstm3 = nn.LSTM(input_size=256, hidden_size=512, num_layers=1, batch_first=True)
-        self.dropout1 = nn.Dropout(0.1)
-        self.lstm4 = nn.LSTM(input_size=512, hidden_size=256, num_layers=1, batch_first=True)
-        self.lstm5 = nn.LSTM(input_size=256, hidden_size=128, num_layers=1, batch_first=True)
-        self.lstm6 = nn.LSTM(input_size=128, hidden_size=64, num_layers=1, batch_first=True)
-        self.dropout2 = nn.Dropout(0.1)
-        self.lstm7 = nn.LSTM(input_size=64, hidden_size=32, num_layers=1, batch_first=True)
-        self.fc = nn.Linear(32, 2)
-
-    def forward(self, x):
-        x, _ = self.lstm1(x)
-        x, _ = self.lstm2(x)
-        x, _ = self.lstm3(x)
-        x = self.dropout1(x)
-        x, _ = self.lstm4(x)
-        x, _ = self.lstm5(x)
-        x, _ = self.lstm6(x)
-        x = self.dropout2(x)
-        x, _ = self.lstm7(x)
-        x = self.fc(x[:, -1, :])
-        return x
-
-
-class DetectPhone(Node):
-    def __init__(self):
-        super().__init__('phone_detect')
-
-        self.bridge = CvBridge()
-
-        self.yolo = YOLO(config['phone_yolo_model'])
-
-        self.labels = self.yolo.names
-        self.colors = [[np.random.randint(0, 255) for _ in range(3)] for _ in range(len(self.labels))] 
-
-        self.model = skeleton_LSTM()
-        self.model.load_state_dict(torch.load(config['phone_lstm_model'], map_location="cpu"))
-        self.model.eval()
-        print("success model load")
-
-    
-    def pose_estimation(self, img):
-        
-        global xy_list_list
-
-        length = 20
-        dataset = []
-        status = 'None'
-        
-        img = cv2.resize(img, (640, 640))
-
-        results = mp_pose_pose.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-        
-        if not results.pose_landmarks: 
-            pass    
-
-        else:
-            xy_list = []
-            idx = 0
-            draw_line_dic = {}
-            
-            for x_and_y in results.pose_landmarks.landmark:
-                if idx in attention_dot:
-                    xy_list.append(x_and_y.x)
-                    xy_list.append(x_and_y.y)
-                    x, y = int(x_and_y.x * 640), int(x_and_y.y * 640)
-                    draw_line_dic[idx] = [x, y]
-                idx += 1
-            xy_list_list.append(xy_list)
-
-            for line in draw_line:
-                x1, y1 = draw_line_dic[line[0]][0], draw_line_dic[line[0]][1]
-                x2, y2 = draw_line_dic[line[1]][0], draw_line_dic[line[1]][1]
-                img = cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 4)
-
-            
-            phone_results = self.yolo(img, stream=True)
-
-            for r in phone_results:
-                
-                annotator = Annotator(img)
-
-                boxes = r.boxes
-                
-                for idx, box in enumerate(boxes):
-                    b = box.xyxy[0]
-                    c = box.cls
-                
-                    if int(c) == 67:
-                        
-                        color = self.colors[int(c)]
-                        annotator.box_label(b, self.yolo.names[int(c)], color)
-
-                        b_list = b.tolist()
-                        
-                        for idx in range(4):
-                            xy_list.append(b_list[idx]/640.0)
-                        break
-                        
-                    else:
-                        if (len(boxes) - 1 == idx):
-                            for _ in range(4):
-                                xy_list.append(0.0)
-                        else:
-                            continue
-                            
-            img = annotator.result()
-
-            if len(xy_list_list) == length:
-                
-                dataset = []
-                dataset.append({'key' : 0, 'value' : xy_list_list})
-                dataset = MyDataset(dataset)
-                dataset = DataLoader(dataset)
-                xy_list_list = []
-
-                for data, label in dataset:
-                    data = data.to("cuda")
-                    
-                    with torch.no_grad():
-                        self.model = self.model.to("cuda")
-                        result = self.model(data)
-                        _, out = torch.max(result, 1)
-                        
-                        print(out.item())
-                        if out.item() == 0: 
-                            status = 'phone'
-                            
-                        else: 
-                            status = 'work'
-
-        return img, status
-    
-    
-class Camera(QThread):
-    update = pyqtSignal()
-
-    def __init__(self, sec=0, parent=None):
-        super().__init__()
-        self.main = parent
-        self.running = True
-
-
-    def run(self):
-        count = 0
-        while self.running == True:
-            self.update.emit()
-            time.sleep(0.05)
-
-    def stop(self):
-        self.running == False
+from_class = uic.loadUiType(config['GUI'])[0]
 
 
 class WindowClass(QMainWindow, from_class):
@@ -220,32 +35,57 @@ class WindowClass(QMainWindow, from_class):
         self.setupUi(self)
         
         self.bridge = CvBridge()
-        
-        self.record = Camera(self)
-        self.record.deamon = True
-        self.record.update.connect(self.updateRecording)
-        
-        self.play = Camera(self)
-        self.play.daemon = True
 
+        '---------flag------------'
         self.isDetectPhoneOn = False
+        self.isDetectSnackOn = False
+        self.isDetectLightOn = False
+        self.isDetectDoorOn = False
+        self.isDetectDeskOn = False        
+
         self.detectphone = DetectPhone()
+        self.detectdoor = Detectdoor()
+        self.detectlight = Detectlight()
+        self.detectsnack = Detectsnack()
+        self.detectdesk = DetectDesk()
         
-        self.isDetectDeskOn = False
-        self.detectdesk = DetectDesk.DetectDesk()
-        
-        self.detectphone.create_subscription(
+        '--------subscription----------'
+        self.phone_sub = self.detectphone.create_subscription(
         Image,
         '/image_raw',
         self.image_callback,
         1)
+        self.phone_sub
 
-        self.detectdesk.create_subscription(
+        self.door_sub = self.detectdoor.create_subscription(
         Image,
         '/image_raw',
         self.image_callback,
         1)
+        self.door_sub
 
+        self.light_sub = self.detectlight.create_subscription(
+        Image,
+        '/image_raw',
+        self.image_callback,
+        1)
+        self.light_sub
+
+        self.snack_sub = self.detectsnack.create_subscription(
+        Image,
+        '/image_raw',
+        self.image_callback,
+        1)
+        self.snack_sub
+
+        self.desk_sub = self.detectdesk.create_subscription(
+        Image,
+        '/image_raw',
+        self.image_callback,
+        1)
+        self.desk_sub
+
+        '--------------utils---------'
         self.pixmap = QPixmap()
         
         self.timer = QTimer(self)
@@ -255,12 +95,14 @@ class WindowClass(QMainWindow, from_class):
         '-----------camera-------------'
         self.fx_button_phone.clicked.connect(self.click_detect_phone)
         self.fx_button_desk.clicked.connect(self.click_detect_desk)
+        self.fx_button_snack.clicked.connect(self.click_detect_snack)
+        self.fx_button_door.clicked.connect(self.click_detect_door)
+        self.fx_button_light.clicked.connect(self.click_detect_light)
         
         '-------------DB---------------'
         self.set_combo()
         self.db_button_search.clicked.connect(self.search)
         self.db_tableWidget.itemDoubleClicked.connect(self.selectVideo)
-        self.fx_button_play.clicked.connect(self.controlVideo)
         
         
     def set_combo(self):
@@ -301,9 +143,6 @@ class WindowClass(QMainWindow, from_class):
             
             
     def showThumbnail(self):
-        self.pixmap = QPixmap()
-        self.video.setPixmap(self.pixmap)
-        
         ret, frame = self.videoCapture.read()
         
         if ret:
@@ -317,47 +156,7 @@ class WindowClass(QMainWindow, from_class):
             self.pixmap = self.pixmap.scaled(self.label.width(), self.label.height())
             
             self.video.setPixmap(self.pixmap)
-            
-    
-    def controlVideo(self):
-        if self.fx_button_play.text() == "▶":
-            self.playVideo()
-        else:
-            self.pauseVideo()
-            
-            
-    def playVideo(self):
-        self.isVideoEnd = False
-        self.fx_button_play.setText("❚❚")
         
-        while self.isVideoEnd == False:
-            ret, frame = self.videoCapture.read()
-            
-            if not ret:
-                self.isVideoEnd = True
-                self.fx_button_play.setText("▶")
-                break
-            
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            
-            h, w, c = frame.shape
-            bytes_per_line = 3 * w
-            self.qimage = QImage(frame.data, w, h, bytes_per_line, QImage.Format_RGB888)
-
-            self.pixmap = QPixmap.fromImage(self.qimage)
-            self.video.setPixmap(self.pixmap)
-            
-            QApplication.processEvents()  # prevent GUI freeze
-
-            time.sleep(0.05)  # 저장된 동영상에 맞게 setting
-
-        self.videoCapture.release()
-        
-        
-    def pauseVideo(self):
-        self.isVideoEnd = True
-        self.fx_button_play.setText("▶")
-
 
     def image_callback(self, msg):
         cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
@@ -365,12 +164,20 @@ class WindowClass(QMainWindow, from_class):
         if self.isDetectPhoneOn == True:
             img, status = self.detectphone.pose_estimation(cv_image)
             cv2.putText(img, status, (0, 50), cv2.FONT_HERSHEY_COMPLEX, 1.5, (0, 0, 255), 2)
-            self.updateRecording(img)
             
         elif self.isDetectDeskOn == True:
             img, result = self.detectdesk.detect_desk(cv_image)
             self.desk_result += result
-            self.updateRecording(img)
+            self.writer.write(img)
+
+        elif self.isDetectDoorOn == True:
+            img = self.detectdoor.detect_door(cv_image)
+
+        elif self.isDetectSnackOn == True:
+            img = self.detectsnack.detect_snack(cv_image)
+
+        elif self.isDetectLightOn == True:
+            img = self.detectlight.detect_light(cv_image)   
         
         log.info(img.shape)
         
@@ -416,7 +223,6 @@ class WindowClass(QMainWindow, from_class):
             self.fx_button_phone.hide()
             
             self.start_rec_and_req('DESK')
-            self.desk_result = ""
 
         else:
             self.fx_button_desk.setText('DESK')
@@ -426,22 +232,76 @@ class WindowClass(QMainWindow, from_class):
             self.fx_button_snack.show()
             self.fx_button_phone.show()
             
-            self.stop_rec_and_res(self.desk_result)
+    
+    def click_detect_door(self):
+        if self.isDetectDoorOn == False:
+            self.fx_button_door.setText('STOP')
+            self.isDetectDoorOn = True
+            self.fx_button_light.hide()
+            self.fx_button_phone.hide()
+            self.fx_button_desk.hide()
+            self.fx_button_snack.hide()
+
+            self.start_rec_and_req('DOOR')
+        
+        else:
+            self.fx_button_door.setText('DOOR')
+            self.isDetectDoorOn = False
+            self.fx_button_light.show()
+            self.fx_button_phone.show()
+            self.fx_button_desk.show()
+            self.fx_button_snack.show()
+
+
+    def click_detect_light(self):
+        if self.isDetectLightOn == False:
+            self.fx_button_light.setText('STOP')
+            self.isDetectLightOn = True
+            self.fx_button_phone.hide()
+            self.fx_button_door.hide()
+            self.fx_button_desk.hide()
+            self.fx_button_snack.hide()
+
+            self.start_rec_and_req('LIGHT')
+
+        else:
+            self.fx_button_light.setText('LIGHT')
+            self.isDetectLightOn = False
+            self.fx_button_phone.show()
+            self.fx_button_door.show()
+            self.fx_button_desk.show()
+            self.fx_button_snack.show()
+
+
+    def click_detect_snack(self):
+        if self.isDetectSnackOn == False:
+            self.fx_button_snack.setText('STOP')
+            self.isDetectSnackOn = True
+            self.fx_button_light.hide()
+            self.fx_button_door.hide()
+            self.fx_button_desk.hide()
+            self.fx_button_phone.hide()
+
+            self.start_rec_and_req('SNACK')
+
+        else:
+            self.fx_button_snack.setText('SNACK')
+            self.isDetectSnackOn = False
+            self.fx_button_light.show()
+            self.fx_button_door.show()
+            self.fx_button_desk.show()
+            self.fx_button_phone.show()
             
-            
+
     def start_rec_and_req(self, module):
         self.req_id = data_manager.insert_req(module)
             
-        now = dt.now().strftime("%Y%m%d_%H%M%S")
+        now = dt.now().strftime("%Y%m%d_%H%M")
         self.video_path = config['video_dir'] + now + ".avi"
         self.fourcc = cv2.VideoWriter_fourcc(*"XVID")
         self.writer = cv2.VideoWriter(self.video_path, self.fourcc, 60.0, (640, 640))
         
         self.desk_result = ""
-        
-        
-    def updateRecording(self, img):
-        self.writer.write(img)
         
         
     def stop_rec_and_res(self, result):
@@ -458,15 +318,31 @@ class WindowClass(QMainWindow, from_class):
     def spin_node(self):
         if self.isDetectPhoneOn == True:
             rclpy.spin_once(self.detectphone)
+
         elif self.isDetectDeskOn == True:
             rclpy.spin_once(self.detectdesk)
-        # else:
-            # print("not detect mode")
+
+        elif self.isDetectSnackOn == True:
+            rclpy.spin_once(self.detectsnack)
+
+        elif self.isDetectLightOn == True:
+            rclpy.spin_once(self.detectlight)
+
+        elif self.isDetectDoorOn == True:
+            rclpy.spin_once(self.detectdoor)
+        else:
+            print("not detect mode")
     
 
     def shutdown_ros(self):
         print("shutting down ROS")
+
         self.detectphone.destroy_node()
+        self.detectlight.destroy_node()
+        self.detectdoor.destroy_node()
+        self.detectsnack.destroy_node()
+        self.detectdesk.destroy_node()
+
         rclpy.shutdown()  
 
 
