570e4f057fcc71c97df067b2df7eb802814a251e
YunOh21 <yun5.dev@gmail.com>
2023-12-13 10:59:31 +0900

    chore : 변수명 변경

--- ros_dl/src/haejo_pkg/haejo_pkg/utils/ConfigUtil.py
+++ ros_dl/src/haejo_pkg/haejo_pkg/utils/ConfigUtil.py
@@ -1,8 +1,8 @@
 import configparser
 
 def get_config():
-    config = configparser.ConfigParser()
-    config.read('/home/yoh/deeplearning-repo-4/ros_dl/src/haejo_pkg/haejo_pkg/utils/config.ini')
-    dev = config['yun']
+    configParser = configparser.ConfigParser()
+    configParser.read('/home/yoh/deeplearning-repo-4/ros_dl/src/haejo_pkg/haejo_pkg/utils/config.ini')
+    config = configParser['yun']
     
-    return dev
+    return config
